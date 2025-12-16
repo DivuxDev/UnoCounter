53f9007f1cca66b1cd9682399c92474d7ea0562c
DESKTOP-AVDACQ5\david <davidprado228@gmail.com>
2025-12-16 22:24:04 +0100

    fix: update json-server import to use createRequire

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
-import jsonServer from "json-server";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const jsonServer = require("json-server");
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
